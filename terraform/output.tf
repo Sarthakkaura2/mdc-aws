488af9d3b949d6a834e33884e3cd5b2ac4b05b81
Sarthakkaura2 <87518999+Sarthakkaura2@users.noreply.github.com>
2025-10-18 23:03:47 +0530

    Fix formatting in output.tf for stackset outputs

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,7 @@
 output "stackset_name" {
-value = aws_cloudformation_stack_set.mdc_org.name
+  value = aws_cloudformation_stack_set.mdc_org.name
 }
 
-
 output "stackset_id" {
-value = aws_cloudformation_stack_set.mdc_org.id
+  value = aws_cloudformation_stack_set.mdc_org.id
 }
