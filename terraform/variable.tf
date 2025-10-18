f5acf93da5386472a96ddbe9084e841baa1a2955
Sarthakkaura2 <87518999+Sarthakkaura2@users.noreply.github.com>
2025-10-18 23:04:28 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,56 +1,48 @@
 variable "aws_region" {
-description = "AWS region for StackSet admin operations"
-type = string
-default = "us-east-1"
+  description = "AWS region for StackSet admin operations"
+  type        = string
+  default     = "us-east-1"
 }
-
 
 variable "template_path" {
-description = "Local path to the CloudFormation template to deploy (StackSet)"
-type = string
-default = "${path.module}/templates/aws-org-onboarding.yaml"
+  description = "Path to CloudFormation template to deploy"
+  type        = string
+  default     = "${path.module}/templates/aws-org-onboarding.yaml"
 }
-
 
 variable "aws_management_account_id" {
-description = "AWS management account id where StackSet is created"
-type = string
+  description = "AWS management account id"
+  type        = string
 }
-
 
 variable "aws_organization_id" {
-description = "AWS Organization ID (o-xxxx)"
-type = string
+  description = "AWS Organization ID (o-xxxx)"
+  type        = string
 }
-
 
 variable "azure_subscription_id" {
-description = "Azure subscription id used for creating the MDC connector"
-type = string
+  description = "Azure subscription id for connector creation"
+  type        = string
 }
-
 
 variable "azure_resource_group" {
-description = "Resource group where the multi-cloud connector resource will live"
-type = string
+  description = "Azure RG for MDC connector"
+  type        = string
 }
-
 
 variable "connector_location" {
-description = "Azure location for the connector resource"
-type = string
-default = "centralus"
+  description = "Azure region for connector"
+  type        = string
+  default     = "centralus"
 }
-
 
 variable "defender_role_name" {
-description = "Role name created in member account for MDC to assume"
-type = string
-default = "DefenderForCloud-Connector-Role"
+  description = "Name of MDC role in member accounts"
+  type        = string
+  default     = "DefenderForCloud-Connector-Role"
 }
 
-
 variable "azure_management_role_arn" {
-description = "ARN of role created in management account that MDC will reference (used in connector body)"
-type = string
+  description = "Role ARN created in AWS management account"
+  type        = string
 }
