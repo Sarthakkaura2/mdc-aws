4518426a8a5db0d37c5350a2c1ae730d9c067af7
Sarthakkaura2 <87518999+Sarthakkaura2@users.noreply.github.com>
2025-10-19 12:54:28 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -19,7 +19,7 @@ variable "aws_management_account_id" {
 variable "aws_organization_id" {
   description = "AWS Organization ID (o-xxxx)"
   type        = string
-  default     = "o-z693l41vfh"
+  default     = "r-t6wu"
 }
 
 variable "azure_subscription_id" {
