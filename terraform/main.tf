e3d5e2bbbf1a169fe04fb4df79f5e0a91b2db31a
Sarthakkaura2 <87518999+Sarthakkaura2@users.noreply.github.com>
2025-10-19 12:55:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "aws_cloudformation_stack_set_instance" "mdc_org_instance" {
   deployment_targets {
     organizational_unit_ids = [var.aws_organization_id]
   }
-    regions         = ["eu-west-1"]
+    region         = "eu-west-1"
   }
 
 # -----------------------------------------------------------------------------
