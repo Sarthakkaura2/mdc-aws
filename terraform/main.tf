fba4e4835b3bec5d5786f992ee60b0747ee6f7d9
Sarthakkaura2 <87518999+Sarthakkaura2@users.noreply.github.com>
2025-10-19 12:24:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      # Set a modern version, such as the latest major version (v5.0 or later)
-      version = "~> 5.0" 
-    }
-  }
-}
+
 
 locals {
   # StackSet name composed of a prefix and the organization ID
