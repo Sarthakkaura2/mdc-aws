64d03b71dfc97eba99ad417e4a147957acccb626
Sarthakkaura2 <87518999+Sarthakkaura2@users.noreply.github.com>
2025-10-19 10:41:17 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,13 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
   }
-  required_version = ">= 1.5"
+  required_version = ">= 1.5.0"
 }
+
 
 provider "aws" {
   alias  = "management"
