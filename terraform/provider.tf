52c6971e755cd016b70cc84d44a996ab3ad046af
Sarthakkaura2 <87518999+Sarthakkaura2@users.noreply.github.com>
2025-10-19 12:25:09 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      # Set a modern version, such as the latest major version (v5.0 or later)
+      version = "~> 5.0" 
     }
   }
-  required_version = ">= 1.5.0"
 }
 
 
