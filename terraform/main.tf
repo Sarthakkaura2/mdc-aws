9b92a892102f12edeb22e5b0cb93ea70b2e03bba
Sarthakkaura2 <87518999+Sarthakkaura2@users.noreply.github.com>
2025-10-21 21:51:27 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,9 +56,41 @@ resource "aws_cloudformation_stack_set_instance" "mdc_org_instance" {
 resource "null_resource" "create_azure_connector" {
   provisioner "local-exec" {
     command = <<EOT
+export AWS_MGMT_ACCOUNT_ID="${var.aws_management_account_id}"
+export AZURE_MGMT_ROLE_ARN="${var.azure_management_role_arn}"
+export AWS_ORG_ID="${var.aws_organization_id}"
+export STACKSET_ARN="${local.stack_set_admin_role_arn}"
+
 az rest --method put \
-  --url "https://management.azure.com/subscriptions/${var.azure_subscription_id}/resourceGroups/${var.azure_resource_group}/providers/Microsoft.Security/multiCloudConnectors/awsOrgConnector?api-version=2023-10-01-preview" \
-  --body '{"location":"${var.connector_location}","properties":{"cloudName":"AWS","environmentType":"awsOrganization","hierarchyIdentifier":"${var.aws_organization_id}","authenticationDetails":{"authenticationType":"awsAssumeRole","roleArn":"${var.azure_management_role_arn}"}}}'
+  --url "https://management.azure.com/subscriptions/${var.azure_subscription_id}/resourceGroups/${var.azure_resource_group}/providers/Microsoft.Security/securityConnectors/awsOrgConnector?api-version=2024-03-01-preview" \
+  --body '
+    {
+      "location": "${var.connector_location}",
+      "properties": {
+        "cloudName": "AWS",
+        "hierarchyIdentifier": "${AWS_ORG_ID}",
+        "authenticationDetails": {
+          "authenticationType": "awsAssumeRole",
+          "roleArn": "${AZURE_MGMT_ROLE_ARN}"
+        },
+        "organizationalData": {
+          "organizationMembershipType": "Master",
+          "masterAccountId": "${AWS_MGMT_ACCOUNT_ID}",
+          "stacksetArn": "${STACKSET_ARN}"
+        },
+        "offerings": [
+          {
+            "offeringType": "CspmMonitorAwsOrganization",
+            "description": "CSPM for AWS Organization"
+          },
+          {
+            "offeringType": "CspmMonitorAws",
+            "description": "CSPM for all Member Accounts"
+          }
+        ]
+      }
+    }
+'
 EOT
   }
   
